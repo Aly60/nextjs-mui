259257728b8c19ed11239f0bcddcf212a046bf27
Aly60 <alyashmawy98@gmail.com>
2024-06-05 15:38:20 +0300

    all fixed

--- app/pages/Home/page.jsx
+++ app/pages/Home/page.jsx
@@ -1,5 +1,5 @@
-import Header from "../components/Header";
-import UserTable from "../components/UserTable";
+import Header from "../../components/Header";
+import UserTable from "../../components/UserTable";
 
 export default function Home() {
   return (
